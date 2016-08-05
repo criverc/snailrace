27f67101a09c2a9eaabd192150d1da35597bf879
Carlos Rivera <carlos@superkaos.org>
2016-08-05 18:56:02 +0200

    Initial commit
    snails move about by themselves, hit on the walls and bouncing back!

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # snailrace
 A game of racing snails!!
+
+Not much of a game yet!!!
+
+Things to do:
+
+    1. Users should be able to control lettuces, two keys per user.
+
+    2. Snails should follow letuces. The closer it is the faster it goes.
+       If hit a lettuce bite a bit tnd urn around!
---
+++ snailrace.py
@@ -0,0 +1,47 @@
+import sys, pygame
+import time
+pygame.init()
+
+N=10  # number of snails
+
+size = width, height = 640, 480
+
+background = pygame.image.load ("background.gif")
+screen = pygame.display.set_mode(size)
+
+snail = [ pygame.image.load("snail-normal.gif") for i in range(N) ]
+snailrect = [ snail[i].get_rect() for i in range(N) ]
+speed = [ [i+1, 0] for i in range(N) ]
+
+for i in xrange(N):
+    snailrect[i] = snailrect[i].move([0, i*(height/N)])
+
+while 1:
+    screen.blit(background, background.get_rect())
+
+    for i in xrange (N):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        snailrect[i] = snailrect[i].move(speed[i])
+        if snailrect[i].right > width-50 and speed[i][0]>0:
+            snail[i] = pygame.image.load("snail-surprised.gif")
+
+        if snailrect[i].left < 0 or snailrect[i].right > width:
+            snail[i] = pygame.image.load("snail-normal.gif")
+            speed[i][0] = -speed[i][0]
+            if speed[i][0] < 0:
+                snail[i] = pygame.transform.flip (snail[i], True, False)
+
+        if snailrect[i].left < 50 and speed[i][0]<0:
+            snail[i] = pygame.image.load("snail-surprised.gif")
+            snail[i] = pygame.transform.flip (snail[i], True, False)
+
+        if snailrect[i].top < 0 or snailrect[i].bottom > height:
+            speed[i][1] = -speed[i][1]
+
+        screen.blit(snail[i], snailrect[i])
+
+    pygame.display.flip()
+
+    time.sleep (0.01)
