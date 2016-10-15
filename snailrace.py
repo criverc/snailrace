e5feee712e3a511d9fa31bd2137e9f6068a26657
Carlos Rivera <carlos@superkaos.org>
2016-10-15 13:34:51 +0200

    Put lettuce into a class, add set of lettuce gifs
    This is the first step to have lettuces eaten by snails

--- snailrace.py
+++ snailrace.py
@@ -6,14 +6,59 @@ N=4  # number of snails
 
 size = width, height = 640, 480
 
+
+class lettuce (object):
+
+
+    def __init__ (self):
+        self.sizes = [0, 10, 30, 50, 70, 90, 100]
+        self.current_size = self.sizes.pop ()
+        self.rect = None
+        self.__load_image ()
+
+
+    def __load_image (self):
+        self.surface = pygame.image.load ('lettuce_%d%%.gif' % self.current_size)
+        if self.rect is None:
+            self.rect = self.surface.get_rect ()
+
+
+    def take_a_bite (self):
+        try:
+            self.current_size = self.sizes.pop ()
+
+        except IndexError:
+            pass
+
+        self.__load_image ()
+
+
+    def move (self, speed):
+        self.rect = self.rect.move (speed)
+
+
+    @property
+    def left (self):
+        return self.rect.left
+
+
+    @property
+    def right (self):
+        return self.rect.right
+
+
+    def blit (self, screen):
+        screen.blit (self.surface, self.rect)
+
+
 background = pygame.image.load ("background.gif")
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Snail Race!!')
 
 snail = [ pygame.image.load("snail-normal.gif") for i in range(N) ]
 snailrect = [ snail[i].get_rect() for i in range(N) ]
-lettuce = [ pygame.image.load ("lettuce.gif") for i in range(N) ]
-lettuce_rect = [ lettuce[i].get_rect() for i in range(N) ]
+lettuces = [ lettuce () for i in range(N) ]
+
 
 speed = [ [1, 0] for i in range(N) ]
 
@@ -28,7 +73,7 @@ lettuce_keys = { pygame.K_1 : [ 0, [-5, 0] ],
 
 for i in xrange(N):
     snailrect[i] = snailrect[i].move([0, i*(height/N)])
-    lettuce_rect[i] = lettuce_rect[i].move([(width/2), i*(height/N)])
+    lettuces[i].move([(width/2), i*(height/N)])
 
 while 1:
     screen.blit(background, background.get_rect())
@@ -40,13 +85,13 @@ while 1:
             if event.type == pygame.KEYDOWN:
                 if lettuce_keys.has_key (event.key):
                     j, move = lettuce_keys[event.key]
-                    lettuce_rect[j] = lettuce_rect[j].move(move)
+                    lettuces[j].move(move)
 
         snailrect[i] = snailrect[i].move(speed[i])
-        if snailrect[i].right > lettuce_rect[i].left-50 and speed[i][0]>0:
+        if snailrect[i].right > lettuces[i].left-50 and speed[i][0]>0:
             snail[i] = pygame.image.load("snail-surprised.gif")
 
-        if snailrect[i].left < 0 or snailrect[i].right > lettuce_rect[i].left:
+        if snailrect[i].left < 0 or snailrect[i].right > lettuces[i].left:
             snail[i] = pygame.image.load("snail-normal.gif")
             speed[i][0] = -speed[i][0]
             if speed[i][0] < 0:
@@ -60,7 +105,7 @@ while 1:
             speed[i][1] = -speed[i][1]
 
         screen.blit(snail[i], snailrect[i])
-        screen.blit(lettuce[i], lettuce_rect[i])
+        lettuces[i].blit (screen)
 
     pygame.display.flip()
 
